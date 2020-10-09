2e1a998c589f7a43a5f40bb64619b7099b6bb5df
Franco <fran.raggio@hotmail.com>
2020-10-08 23:55:34 -0300

    Componente de Inicio y su css

---
+++ src/modules/user/auth/components/Inicio.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./../styles/Inicio.css";
+class Inicio extends React.Component{
+    render(){
+        return(
+            <div className="padre"> </div>
+        )
+    }
+}
+export default Inicio
