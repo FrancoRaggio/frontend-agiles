20374fcdb41c90decfafdb0a70246e4d05916fd1
Franco <fran.raggio@hotmail.com>
2020-10-08 23:38:53 -0300

    vista del Layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import Auth from "./modules/user/auth/pages/Auth";
 import Register from "./modules/user/auth/pages/Register";
+import Search from "./modules/user/auth/components/Search";
+import Navs from "./modules/user/auth/components/Navs";
+import Logo from "./modules/user/auth/components/Logo";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -18,3 +21,18 @@ function App() {
 }
 
 export default App;
+export class Home extends React.Component {
+  render(){
+    return(
+      <div className="app container">
+        <div className="jumbotron">
+          <p className="lead text-center"> <Logo/></p>
+              <Navs/>
+              <p></p>
+              <Search/>
+        </div>
+      </div>
+    );
+  }
+}
+
---
+++ src/modules/user/auth/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logoimg from '../../../../assets/logo/logo.png'
+
+class Logo extends React.Component{
+    render(){
+        return(
+            <div>
+                <img height="200" src={logoimg} />
+            </div>
+        )
+    }
+}
+export default Logo
