0bae66eaa758a6ddc1abc4c271b7944ea7ef3f49
Sol99 <caceressol@hotmail.com.ar>
2020-10-27 01:09:29 -0300

    arreglando merge

--- src/modules/layouts/Footer.js
+++ src/modules/layouts/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import perritos from "../../assets/img/perritos.png";
 import { Container, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import "../user/styles/footer.css"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,15 +11,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 const Footer = () => {
   const classes = useStyles();
   return (
-    <Grid style={{ position: 'relative', zIndex: '0' }} className={classes.root}>
+    
       <Grid xs={12} sm={12}>
         <img width="100%" heigth="50px" src={perritos} alt="perritos" />
       </Grid>
-    </Grid>
+    
+
   );
 };
 
+
 export default Footer;
+
+
+
