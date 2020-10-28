0324ca99714d9034f672c9a9480fd73bed13a219
Franco <fran.raggio@hotmail.com>
2020-10-27 23:10:09 -0300

    Arreglos del diseño del home

--- src/modules/layouts/Card.js
+++ src/modules/layouts/Card.js
@@ -6,17 +6,16 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Link, useHistory } from "react-router-dom";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
@@ -41,25 +40,23 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function RecipeReviewCard({item}) {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
 
+  const classes = useStyles();
+
+  const [expanded, setExpanded] = React.useState(false);
+  const [color, setColor] = React.useState(false);
+
+  const history = useHistory();
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-  console.log(item)
+ 
   return (
     <Card className={classes.root}>
       <CardHeader
         avatar={
-          <Avatar aria-label="recipe" src={item?.user?.avatar} className={classes.avatar}>
-            
+          <Avatar aria-label="recipe" src={item?.user?.avatar} className={classes.avatar}>           
           </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
         }
         title={item.title}
         subheader={item.date}
@@ -77,35 +74,25 @@ export default function RecipeReviewCard({item}) {
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
         </IconButton>
-        <IconButton aria-label="share">
-          
+        <IconButton aria-label="share">        
         </IconButton>
         <BottomNavigation
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction label="Likes" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Localizacion" icon={<LocationOnIcon />} />
-    </BottomNavigation>
+            showLabels
+            className={classes.root}
+          >
+        <BottomNavigationAction label="Likes" icon={<FavoriteIcon style={{color: color ? "red" : "gray"}} />} onClick={() => setColor(!color) } />
+        <BottomNavigationAction label="Localizacion" icon={<LocationOnIcon style={{color: "#00aae4"}} />}  onClick={() => history.push("/map") } />
+        </BottomNavigation>
         <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
         </IconButton>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Comentarios</Typography>
-          <Typography paragraph>
-            Enviar WS
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
--- src/modules/layouts/Navs.js
+++ src/modules/layouts/Navs.js
@@ -1,40 +1,49 @@
+
 import React from 'react'
 import { Link, useHistory } from "react-router-dom";
 
-class Navs extends React.Component {
-	render() {
-		return (
-			<nav class=" navbar navbar-expand-lg navbar-dark bg-dark ">
-				<Link class="navbar-brand" to={"/"}>Inicio</Link>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
 
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item active">
-							<Link class="nav-link" to={"/advert"}>Anuncios <span class="sr-only">(current)</span></Link>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Publicaciones
-        					</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<Link class="dropdown-item" to={"/add-pet"}>Publicar Mascota</Link>
-								
-							</div>
-						</li>
-						<li class="nav-item">
-							<Link class="nav-link" to={"/about"}>SobreNosotros</Link>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#" >Perfil</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		)
-	}
+class Naavs extends React.Component {
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <Link class="navbar-brand" to={"/"}>Inicio</Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      <Link class="nav-link" to={"/advert"}>Anuncios <span class="sr-only">(current)</span></Link>
+      </li>
+     
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Publicaciones 
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <Link class="dropdown-item" to={"/add-pet"}>Publicar Mascota</Link>
+          </div>
+      </li>
+      <li class="nav-item">
+      <Link class="nav-link" to={"/about"}>SobreNosotros</Link>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Mi perfil <span class="sr-only">(current)</span></a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Buscar..." />
+      <button class="btn btn-outline-success my-5 my-sm-0" type="submit">Buscar</button>
+    </form>
+  </div>
+</nav>
+
+        )
+
+    }
 }
 
-export default Navs;
+export default Naavs;
